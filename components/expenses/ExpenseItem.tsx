7debbf3c5c73da57c8a36b25e08a359ea85f563d
mldamico <matias.lioneldamico@gmail.com>
2023-01-28 09:25:28 -0300

    Date output util function

--- components/expenses/ExpenseItem.tsx
+++ components/expenses/ExpenseItem.tsx
@@ -1,6 +1,7 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { GlobalStyles } from "../../constants/styles";
+import { getFormattedDate } from "../../util/date";
 
 interface Props {
   description: string;
@@ -16,10 +17,10 @@ const ExpenseItem = ({ description, amount, date }: Props) => {
           <Text style={[styles.textBase, styles.description]}>
             {description}
           </Text>
-          <Text style={styles.textBase}>{date.toISOString()}</Text>
+          <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
         </View>
         <View style={styles.amountContainer}>
-          <Text style={styles.amount}>{amount}</Text>
+          <Text style={styles.amount}>{amount.toFixed(2)}</Text>
         </View>
       </View>
     </Pressable>
@@ -57,6 +58,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 4,
+    minWidth: 80,
   },
   amount: {
     color: GlobalStyles.colors.primary500,
